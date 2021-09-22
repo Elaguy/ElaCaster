92b76d1738c585477948ff8edeee723c56ab7268
Elaguy <Elaguy@ELAGUY-WINDOWS->
2021-09-21 21:13:42 -0400

    Add project files.

---
+++ ElaCaster/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace ElaCaster
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RenderWindow window = new RenderWindow(new VideoMode(200, 200), "SFML Test");
+
+            CircleShape circleShape = new CircleShape(100.0f);
+            circleShape.FillColor = Color.Green;
+
+            while(window.IsOpen)
+            {
+                window.Clear();
+                window.Draw(circleShape);
+                window.Display();
+            }
+        }
+    }
+}
